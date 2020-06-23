10b029ed05dfea37b12112107255c92208dc581e
melaniegrigg <melanie.grigger@gmail.com>
2020-06-22 21:55:46 -0400

    ititial commit

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,15 @@
-piano = { color: "black", price: 4000, department: "music" }
-desk = { color: "brown", price: 560, department: "furniture" }
-lamp = { color: "white", price: 75, department: "lighting" }
+# piano = { color: :black, price: 4000, department: :music }
+# desk = { color: :brown, price: 560, department: :furniture }
+# lamp = { color: :white, price: 75, department: :lighting }
+# p piano[color:]
+
+class Item
+  def initialize(color, price, department)
+    @color = color
+    @price = price
+    @department = department
+  end
+end
+
+piano = Item.new("black", 4000, "music")
+desk = Item.new("brown", 560, "furniture")

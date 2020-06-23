cde468ae59b8743107a2479e7a302b7fbea3130e
melaniegrigg <melanie.grigger@gmail.com>
2020-06-23 08:32:38 -0400

    fix writer method

--- store_item.rb
+++ store_item.rb
@@ -12,28 +12,12 @@ class Item
     @price = input_options[:price]
     @department = input_options[:department]
   end
-
-  def color
-    @color
-  end
-
-  def price
-    @price
-  end
-
-  def price=(number)
-    @price = number
-  end
-
-  def department
-    @department
-  end
 end
 
-piano = Item.new("black", 4000, "music")
-desk = Item.new("brown", 560, "furniture")
-lamp = Item.new("white", 75, "lighting")
+piano = Item.new(color: "black", price: 4000, department: "music")
+desk = Item.new(color: "brown", price: 560, department: "furniture")
+lamp = Item.new(color: "white", price: 75, department: "lighting")
 
 p piano.color
-p desk.price
+p desk.price = 70000
 p lamp.department

fcaa82f6808380514622f9a94454a403500e98a2
melaniegrigg <melanie.grigger@gmail.com>
2020-06-22 21:12:52 -0400

    initial commit

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+piano = { :color => "black", :price => 4000, :department => "music" }
+desk = { :color => "brown", :price => 560, :department => "furniture" }
+lamp = { :color => "white", :price => 75, :department => "lighting" }

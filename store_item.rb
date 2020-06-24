82f23d5cb412f0f08a72aa86271ae01bd22e17d0
melaniegrigg <melanie.grigger@gmail.com>
2020-06-23 21:34:32 -0400

    inheritance class add

--- store_item.rb
+++ store_item.rb
@@ -13,6 +13,12 @@ class Item
   end
 end
 
+class LivingRoom < Item
+  def production_date
+    @production_date
+  end
+end
+
 piano = Item.new(color: "black", price: 4000, department: "music")
 desk = Item.new(color: "brown", price: 560, department: "furniture")
 lamp = Item.new(color: "white", price: 75, department: "lighting")
@@ -21,3 +27,6 @@ puts piano.color
 puts desk.price = 70000
 puts lamp.department
 puts lamp.price = 48930293
+
+moving = LivingRoom.new(color: "black", price: 4000, department: "music", production_date: "July 4, 2016")
+p moving.color
